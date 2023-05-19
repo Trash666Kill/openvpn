2491a9b5235206618d34ffb8c437c27900d50188
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-05-19 11:22:06 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -47,6 +47,7 @@ explicit-exit-notify 1
 #
 sysctl -w net.ipv4.ip_forward=1
 sysctl net.ipv4.ip_forward
+iptables -t nat -A POSTROUTING -s 10.8.15.0/24 -o enp1s0 -j MASQUERADE
 #
 ./easyrsa --batch gen-req emperor nopass
 ./easyrsa --batch sign-req client emperor
@@ -57,3 +58,28 @@ cp -v issued/emperor.crt ../../clients/emperor
 cp -v private/emperor.key ../../clients/emperor
 cp -v private/emperor.pem ../../clients/emperor
 #
+{(
+cat <(echo -e 'client') \
+<(echo -e 'proto udp') \
+<(echo -e 'dev tun') \
+<(echo -e 'remote strychnine.duckdns.org 2944') \
+<(echo -e 'resolv-retry infinite') \
+<(echo -e 'nobind') \
+<(echo -e 'persist-key') \
+<(echo -e 'persist-tun') \
+<(echo -e 'remote-cert-tls server') \
+<(echo -e 'cipher AES-256-GCM') \
+<(echo -e '#user nobody') \
+<(echo -e '#group nobody') \
+<(echo -e 'verb 3') \
+    <(echo -e '<ca>') \
+    ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    emperor.crt \
+    <(echo -e '</cert>\n<key>') \
+    emperor.key \
+    <(echo -e '</key>\n<tls-crypt-v2>') \
+    emperor.pem \
+    <(echo -e '</tls-crypt-v2>') \
+    > ../../clients/emperor/emperor.ovpn
+ )}
